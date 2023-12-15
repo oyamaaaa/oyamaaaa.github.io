66f00a6c1520244aa1995ea933a17ee75c945326
oyamaaaa <151658475+oyamaaaa@users.noreply.github.com>
2023-12-15 22:45:41 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.slick').slick({
+        fade: true,
+        autoplay: true,
+        speed: 1500,
+        autoplaySpeed : 2000,
+        pauseOnFocus: false,
+        pauseOnHover: false,
+        arrows: false,
+    })
+});
